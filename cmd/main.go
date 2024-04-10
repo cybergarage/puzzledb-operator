cceaf0c0838c7cb0e8e3b6ede821a50aebd722ff
Satoshi Konno <skonno@cybergarage.org>
2024-04-10 23:07:09 +0900

    Add recorder to controller

--- cmd/main.go
+++ cmd/main.go
@@ -123,8 +123,9 @@ func main() {
 	}
 
 	if err = (&controller.PuzzleDBReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
+		Client:   mgr.GetClient(),
+		Scheme:   mgr.GetScheme(),
+		Recorder: mgr.GetEventRecorderFor("puzzledb-controller"),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "PuzzleDB")
 		os.Exit(1)
