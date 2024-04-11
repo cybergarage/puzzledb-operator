473f1f6dc552109a24af80360f0b17834c73ddc4
Satoshi Konno <skonno@cybergarage.org>
2024-04-11 13:00:30 +0900

    Update PuzzleDBReconciler::Reconcile()

--- internal/controller/puzzledb_controller.go
+++ internal/controller/puzzledb_controller.go
@@ -22,6 +22,7 @@ import (
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
